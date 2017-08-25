9c21fcb32036be1bd2df7b24ba2ac3f239b208da
Jakson Wainwright <jaxonawain@gmail.com>
2017-08-25 11:01:08 -1000

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-function check(form){
-    if(form.uname.value == "Administrator" && form.psd.value == "Ad!12345"){
-        window.open('index.html')
-    }
-    else{
-        alert("Invalid Credentials - Please Try Again")
-    }
-}
